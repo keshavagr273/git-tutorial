5825b9966c5f5b43d4b691c538fa5e4541067874
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-03-25 16:46:46 +0530

    Complex program starts

--- utils/kk.java
+++ utils/kk.java
@@ -1,11 +1,11 @@
 class KK {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 20;
-        int sum = num1 + num2 + 23;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-        int product = num1 * num2;
-        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+        int firstOperand = 10;
+        int secondOperand = 20;
+        int complexSum = firstOperand + secondOperand + 23;
+        System.out.println("The sum of " + firstOperand + " and " + secondOperand + " is: " + complexSum);
+        int complexProduct = firstOperand * secondOperand;
+        System.out.println("The product of " + firstOperand + " and " + secondOperand + " is: " + complexProduct);
 
         if (num2 != 0) {
             int division = num1 / num2;
