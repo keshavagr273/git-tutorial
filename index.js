446e461c31dd03bcab263f5abeba80a08702dbbf
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-03-25 16:09:59 +0530

    Simple Java Program

---
+++ index.js
@@ -0,0 +1,9 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+const firstNumber = 5;
+const secondNumber = 10;
+const sum = addNumbers(firstNumber, secondNumber);
+console.log(`The sum of ${firstNumber} and ${secondNumber} is ${sum}`);
+
