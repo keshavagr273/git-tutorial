215ca1d39b1a5046093a067f3a8de6e8dec02270
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-03-27 18:48:02 +0530

    Stop tracking index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function addNumbers(a, b) {
-    return a + b;
-}
-
-const firstNumber = 5;
-const secondNumber = 10;
-const sum = addNumbers(firstNumber, secondNumber);
-console.log(`The sum of ${firstNumber} and ${secondNumber} is ${sum}`);
-
